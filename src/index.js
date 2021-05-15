33e2d5390b0f4ca4df0b2d24cab326140dfe5495
Saurabh <agrawalsaurabh026@gmail.com>
2021-05-15 20:23:53 +0530

    common webpack configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import sum from './sum.js';
+ 
+console.log(sum(2, 5));
+console.log(process.env.NODE_ENV)
 
 const title = 'React with Webpack and Babel';
 
