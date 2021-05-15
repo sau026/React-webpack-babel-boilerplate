c3f44079ebe5b41ba104f3e33b842dd494901bf2
Saurabh <agrawalsaurabh026@gmail.com>
2021-05-16 03:06:10 +0530

    seperate css img and js folder

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -8,34 +9,38 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx|)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
       {
-        test: /\.(jpe?g|gif|png|svg)$/i,
-        use: {
-          loader: "url-loader",
-        },
-      },
-      {
+        // config for sass compilation
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader",
+            loader: MiniCssExtractPlugin.loader,
           },
-          {
-            loader: "css-loader",
-          },
+          "css-loader",
           {
             loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img',
+            }
           },
         ],
       },
     ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"],
+    extensions: ["*", ".js", ".jsx", ".css", ".scss"],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -44,10 +49,14 @@ module.exports = {
       title: "Hello Webpack bundled JavaScript Project",
       template: path.resolve(__dirname, "..", "./src/index.html"),
     }),
+    new MiniCssExtractPlugin({ // plugin for controlling how compiled css will be outputted and named
+      filename: "css/[name].css",
+      chunkFilename: "css/[id].css"
+    })
   ],
   output: {
     path: path.resolve(__dirname, "..", "./dist"),
-    filename: "bundle.js",
+    filename: "js/bundle.js",
   },
   devServer: {
     contentBase: path.resolve(__dirname, "..", "./dist"),
