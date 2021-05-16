26c96b5ea96bb54fa451bd99b81f3f14bc2f6aee
Saurabh <agrawalsaurabh026@gmail.com>
2021-05-16 19:16:00 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import Image from "./assets/images";
 import IM from './assets/images/sun.jpg' 
+import { BrowserRouter } from "react-router-dom";
+import RouterOutlet from "./router-outlet";
 import "./App.scss";
 
-const App = ({title}) => {
-    return(
-        <div>
-            <h2 className="heading">{title}</h2>
-            <img src={IM}/>
-        </div>
-    )
-}
+// const App = ({title}) => {
+//     return(
+//         <div>
+//             <h2 className="heading">{title}</h2>
+//             <img src={IM}/>
+//         </div>
+//     )
+// }
 
-// const App = ({ title }) => {
-//   return (
-//     <BrowserRouter>
-//       <RouterOutlet />
-//     </BrowserRouter>
-//     // <div>
-//     //     <h2 className="heading">{title}</h2>
-//     //     <img src={IM}/>
-//     //     <Login/>
-//     // </div>
-//   );
-// };
+const App = ({ title }) => {
+  return (
+    <BrowserRouter>
+      <RouterOutlet />
+    </BrowserRouter>
+  );
+};
 
 export default App;
